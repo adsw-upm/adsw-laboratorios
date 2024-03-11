578b4824c6f3468da754b104b48effbfeb740718
migueldemiguel <miguel.demiguel@upm.es>
2024-03-11 16:49:09 +0100

    Laboratorio 2
    Laboratorio 2

---
+++ lab2/src/es/upm/dit/adsw/Bando.java
@@ -0,0 +1,9 @@
+package es.upm.dit.adsw;
+
+/**
+ * Esta clase representa el bando de una pieza de ajedrez. Contiene información
+ * sobre los dos bandos posibles: blancas y negras.
+ */
+public enum Bando {
+	BLANCAS, NEGRAS
+}
