9fc67662e2d291b65e813103d85c6342772b669d
Javier Andion <j.andion@upm.es>
2025-01-27 17:10:37 +0100

    2025

--- lab2/src/es/upm/dit/adsw/Bando.java
+++
@@ -1,9 +0,0 @@
-package es.upm.dit.adsw;
-
-/**
- * Esta clase representa el bando de una pieza de ajedrez. Contiene información
- * sobre los dos bandos posibles: blancas y negras.
- */
-public enum Bando {
-	BLANCAS, NEGRAS
-}
--- lab2/src/es/upm/dit/adsw/TipoPieza.java
+++
@@ -1,9 +0,0 @@
-package es.upm.dit.adsw;
-
-/**
- * Esta clase representa un tipo de pieza de ajedrez. Contiene información sobre
- * los distintos tipos de piezas de ajedrez.
- */
-public enum TipoPieza {
-	PEON, TORRE, CABALLO, ALFIL, REINA, REY;
-}
